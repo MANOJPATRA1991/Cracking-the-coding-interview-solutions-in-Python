52265389eb745f666d62c94144fe7fed04d9db42
Manoj Kumar Patra <patra.manoj0@gmail.com>
2017-10-01 23:57:53 +0530

    Add new solution to string permutation

--- Arrays and Strings/Problem 3/main.py
+++ Arrays and Strings/Problem 3/main.py
@@ -2,9 +2,24 @@
 # Permutation means having same characters but in different order
 
 def permutation(s,t):
+  # SOLUTION I
+  # if (len(s) != len(t)):
+  #   return False
+    
+  # return sorted([c for c in s]) == sorted([c for c in t])
+  
+  # SOLUTION II
   if (len(s) != len(t)):
     return False
     
-  return sorted([c for c in s]) == sorted([c for c in t])
+  letters = [0]*256
+  for c in [i for i in s]:
+    letters[ord(c)] = letters[ord(c)] + 1
+  
+  for c in [j for j in t]:
+    if (letters[ord(c)]-1 < 0):
+      return False
+  
+  return True
 
-print(permutation("GOD", "OGD"))
+print(permutation("GOD", "GOD"))
