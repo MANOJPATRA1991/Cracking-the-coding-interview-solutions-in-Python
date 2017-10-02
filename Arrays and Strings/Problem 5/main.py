3ac1764f2f44b04823f45174af675adc74c37761
Manoj Kumar Patra <patra.manoj0@gmail.com>
2017-10-03 01:18:24 +0530

    Add method to replace all spaces in a string

---
+++ Arrays and Strings/Problem 5/main.py
@@ -0,0 +1,27 @@
+# write a method to replace all spaces in a string with "%20"
+# Given: True length of the string
+def replaceSpaces(str, length):
+
+  # SOLUTION I
+  spaceCount = 0
+  numLength = 0
+  listArr = [c for c in str]
+  for c in str:
+    if c == ' ':
+      spaceCount = spaceCount + 1
+  numLength = length + spaceCount*2
+  listArr = listArr + ['0']*spaceCount*2
+  for i in range (length-1, 0, -1):
+    if listArr[i] == ' ':
+      listArr[numLength-1] = '0'
+      listArr[numLength-2] = '2'
+      listArr[numLength-3] = '%'
+      numLength = numLength - 3
+    else:
+      listArr[numLength-1] = listArr[i]
+      numLength = numLength-1
+      
+  return ''.join(listArr)    
+  
+print(replaceSpaces("I am a human", 12))  
+   
