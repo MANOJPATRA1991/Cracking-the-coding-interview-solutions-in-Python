8994f8594e752fe9d155f08b36067819fd77b21f
Manoj Kumar Patra <patra.manoj0@gmail.com>
2017-10-03 15:38:41 +0530

    Add complexity details

--- Arrays and Strings/Problem 1/main.py
+++ Arrays and Strings/Problem 1/main.py
@@ -2,7 +2,10 @@
 import sys
 def isUniqueString(str):
   # Solution I
-  
+  #
+  # TIME COMPLEXITY = O(n), n = length of the string
+  # SPACE COMPLEXITY = O(1)
+  #
   # # if length of string is greater than 128,
   # # it definitely contains duplicate characters
   # if len(str) > 128:
@@ -17,26 +20,29 @@ def isUniqueString(str):
   # # unique characters in string
   # return True
   
-  # Solution II
   
+  # Solution II
   # return len(set(str)) == len(str)
   
+  
   # Solution III
-  
-  checker = 0
-  
-  for c in str:
-    val = ord(c) - ord('a')
-    try:
-      # if character position already 1 in checker,
-      # value of if conditional will be true
-      if ((checker & (1 << val)) > 0):
-        return False
-      # update particular bit in checker as per position of c
-      checker = checker | (1 << val)
-    except ValueError as v:
-      continue
-  return True
+  #
+  # TIME COMPLEXITY = O(n), n = length of the string
+  # SPACE COMPLEXITY = O(1/8)
+  #
+  # checker = 0
+  # for c in str:
+  #   val = ord(c) - ord('a')
+  #   try:
+  #     # if character position already 1 in checker,
+  #     # value of if conditional will be true
+  #     if ((checker & (1 << val)) > 0):
+  #       return False
+  #     # update particular bit in checker as per position of c
+  #     checker = checker | (1 << val)
+  #   except ValueError as v:
+  #     continue
+  # return True
   
 print(isUniqueString("abc de"))
 print(isUniqueString("12edfc"))
