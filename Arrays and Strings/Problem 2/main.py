ca391fb346aadedda9f7cf0eddc6cadd0ab5e8f3
Manoj Kumar Patra <patra.manoj0@gmail.com>
2017-10-03 20:27:50 +0530

    Add recursive solution

--- Arrays and Strings/Problem 2/main.py
+++ Arrays and Strings/Problem 2/main.py
@@ -8,12 +8,18 @@ def rev(str):
   # return str[::-1]
   
   # SOLUTION III
-  begin, end = 0, len(str)-1
-  revStr = [c for c in str]
-  while (begin < end):
-    revStr[end], revStr[begin] = revStr[begin], revStr[end]
-    begin = begin + 1
-    end = end - 1
-  return ''.join(revStr)
+  #   begin, end = 0, len(str)-1
+  #   revStr = [c for c in str]
+  #   while (begin < end):
+  #     revStr[end], revStr[begin] = revStr[begin], revStr[end]
+  #     begin = begin + 1
+  #     end = end - 1
+  #   return ''.join(revStr)
+  
+  # SOLUTION IV
+  #   if str1 != "":
+  #         return str1[-1:] + rev(str1[:-1])
+  #     else:
+  #         return ""
   
 print(rev("flash"))
