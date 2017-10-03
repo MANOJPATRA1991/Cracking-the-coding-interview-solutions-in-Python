395a052d111c6acc8beb3910fae59210ba10cd96
Manoj Kumar Patra <patra.manoj0@gmail.com>
2017-10-03 20:44:15 +0530

    Add minor update

--- Arrays and Strings/Problem 5/iterSol.py
+++ Arrays and Strings/Problem 5/iterSol.py
@@ -1,6 +1,10 @@
 import itertools
 
 def compress(s):
-  return ''.join('{0}{1}'.format(k, len(list(g))) for k, g in itertools.groupby(s))
-  
-compress('aabcccccaaa')
+  res = ''.join('{0}{1}'.format(k, len(list(g))) for k, g in itertools.groupby(s))
+  if len(res) < len(s):
+    return res
+  else:
+    return s
+    
+print(compress('aabcccccaaa'))
