75483abd121ff067642c83bf7af615f527872a0e
Manoj Kumar Patra <patra.manoj0@gmail.com>
2017-10-08 16:24:05 +0530

    Add solution to this problem

---
+++ Linked Lists/Problem-3/main.py
@@ -0,0 +1,40 @@
+# Implement an algorithm to to delete a node in the middle of a singly linked list, given only access to that node
+
+# TIP: This problem cannot be solved if the given node is the last node
+
+def deleteNode(n):
+  if n is not None and n.next is not None:
+    n.value = n.next.value
+    n.next = n.next.next
+  return
+    
+  
+e1 = Element(1)
+e2 = Element(2)
+e3 = Element(3)
+e4 = Element(4)
+e5 = Element(5)
+e6 = Element(6)
+e7 = Element(7)
+e8 = Element(8)
+e9 = Element(9)
+e10 = Element(10)
+e11 = Element(11)
+e12 = Element(12)
+
+ll = LinkedList(e1)
+ll.append(e2)
+ll.append(e3)
+ll.append(e4)
+ll.append(e5)
+ll.append(e6)
+ll.append(e7)
+ll.append(e8)
+ll.append(e9)
+ll.append(e10)
+ll.append(e11)
+ll.append(e12)
+
+deleteNode(ll.head.next.next)
+deleteNode(ll.head.next.next.next)
+print(ll.size())
