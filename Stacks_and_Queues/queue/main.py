3c11097c6bf41980171c8af3fc32c839ca33e0c0
MANOJ PATRA <patra.manoj0@gmail.com>
2017-10-26 12:33:40 +0530

    Add queue data structure

---
+++ Stacks_and_Queues/queue/main.py
@@ -0,0 +1,19 @@
+# assume rear is at position 0 in the list
+
+class Queue(object):
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    # O(n) performance
+    def enqueue(self, items):
+        self.items.insert(0, items)
+
+    # O(1)
+    def dequeue(self):
+        return self.items.pop()
+
+    def size(self):
+        return len(self.items)
