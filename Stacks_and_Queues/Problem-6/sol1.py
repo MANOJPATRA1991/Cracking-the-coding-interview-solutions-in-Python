c6b1746db99dfcd04d4bd39e931f41ed4e60a5a7
MANOJ PATRA <patra.manoj0@gmail.com>
2017-11-11 07:53:45 +0530

    Add implementation fo sorting a stack using another stack

---
+++ Stacks_and_Queues/Problem-6/sol1.py
@@ -0,0 +1,33 @@
+# Sort a stack in ascending order with the help of only one additional stack
+
+from Stacks_and_Queues.stacks.main import Stack
+
+
+def sort_stack(s):
+    """
+    Sort a stack
+     Args:
+         s(Stack): The stack to sort
+     Returns:
+         Stack: Sorted stack
+    """
+    res = Stack()
+    while not s.is_empty():
+        temp = s.pop()
+        while not res.is_empty() and res.peek() > temp:
+            s.push(res.pop())
+        res.push(temp)
+    return res
+
+x = Stack()
+x.push(5)
+x.push(2)
+x.push(7)
+x.push(3)
+
+x = sort_stack(x)
+
+print(x.pop())
+print(x.pop())
+print(x.pop())
+print(x.pop())
