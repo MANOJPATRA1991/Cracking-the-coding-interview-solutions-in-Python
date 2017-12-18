4ef3231ee2b950299a1a75137c8d274ad0954df3
MANOJ PATRA <patra.manoj0@gmail.com>
2017-12-19 02:49:07 +0530

    Add solution to compute all permutations of a string of unique characters

---
+++ Recursion_and_DP/Problem-5/main.py
@@ -0,0 +1,19 @@
+# A method to compute all permutations of a string of unique characters.
+# O(n!)since there are n! permutations
+
+
+def permutations(s):
+    """
+    Attributes:
+        s(String): String of unique characters
+    """
+    if len(s) == 1:
+        yield s
+
+    for index, letter in enumerate(s):
+        remainder = s[:index] + s[index+1:]
+        for word in permutations(remainder):
+            yield letter + word
+
+result = list(permutations('abc'))
+print(result)
